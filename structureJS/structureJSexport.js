7cea73255cd9649e791537b0c3c6f0991c4662c2
Shamari Feaster <sf11@my.fsu.edu>
2014-04-19 01:39:51 -0400

    added some comments explain how I did the synchronization things b/c I will have no idea in about 12 hours

--- structureJS/structureJSexport.js
+++ structureJS/structureJSexport.js
@@ -7,7 +7,12 @@ structureJS.module({name: 'structureJSexport', type : 'Utility'}, function(requi
   var indexes = {};
   var exportList = {};
   var thisGroup = null;
-  //exports.shift();//take uglify off the front
+  /*Configure
+    tab : spaces | \t
+    newlines : \n | \r\n
+  */
+
+  /*indexes are counters indexed on group name*/
   for(var i = 0; i < exports.length; i++){
     thisGroup = structureJS[exports[i]];
     indexes[exports[i]] = 0;
@@ -25,23 +30,30 @@ structureJS.module({name: 'structureJSexport', type : 'Utility'}, function(requi
   function processFileOutput(input){
     return input.replace(/\n/g,'<br>').replace(/  /g,'&nbsp&nbsp&nbsp&nbsp');
   }
-
+  /*The scoping on this is INSANE. I honestly don't even understand why it works 
+  this way but it does. The callback executes in the context of the xhr request
+  but it has access to the scope of the originating function (combineSrcFiles)
+  so listName stays the same through the whole sequence of xhr requests on
+  a group's files array. This allows me to index on exportList and, more importantly,
+  indexes. This is what creates the separation of the outputs. The synchonization
+  is achieved     Because the scopes of combineSrcFiles and the module
+  remain stable through my execution I am able to separate and synchonize the
+  output from sequential, but interleaving, calls to getSrc.  */
   function combineSrcFiles(listName){
     
     function callback(){
-      
-      //console.log(this.responseText);
-      //var nextFile = exportList[listName].files[indexes[listName]];
-      //indexes[listName] = indexes[listName] + 1;
-      //console.log('Next: ' + nextFile);
+
       if(indexes[listName]++ < exportList[listName].files.length){
         getSrc( exportList[listName], callback);
         console.log('WRITTIGN ' + listName);
+        //console.log(this);
+        //look at the scope of the xhr request for more info
         exportList[listName].output += processFileOutput('\n\n' + this.responseText);
-        //document.write();
         indexes[listName];
       }
-
+      /*note the postfix incrementation. I am able to detect completion, yet not
+      index out of bounds b/c of this. By checking for OOB here I am able to 
+      detect completion*/
       if(indexes[listName] == exportList[listName].files.length){
         console.log(listName + ' Done'); 
         document.write(exportList[listName].output);
@@ -65,6 +77,7 @@ structureJS.module({name: 'structureJSexport', type : 'Utility'}, function(requi
     }
     
   }
+  /*In a way I am kindof spawning and tagging x threads with a group name*/
   for(var name in exportList){
     combineSrcFiles( name );
   }
