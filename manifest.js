ed1a98b6a2d1de399eb1231a096686916547d22a
Shamari Feaster <sf11@my.fsu.edu>
2014-04-26 10:29:38 -0400

    fixed indexing error in structureJS-dependency:deferenceGroups() that was causing error in array mutation, namely it was lopping off the last element. Also note the defense of iteration over recursion in the same function. Reduced memory footprint due to a single activation record

--- manifest.js
+++ manifest.js
@@ -1,5 +1,5 @@
 structureJS.declare('Modules/C');  
-structureJS.declare('Modules/D');
+structureJS.declare('Modules/D',['DB']);
 //structureJS.declare('remote/group-UI');
 //structureJS.declare('D-jquery-dep', ['DB','C']);
 
@@ -14,6 +14,15 @@ structureJS.declareGroup({
     description : 'API for client-side DB functions'
   });  
 
+structureJS.declareGroup({
+    name : 'DB2', 
+    description : 'API for client-side DB functions'
+  });  
+  
+structureJS.declareGroup({
+    name : 'DB3', 
+    description : 'API for client-side DB functions'
+  });
 //populate group  
 structureJS.UI_Events.declare('Modules/MouseListener');
 structureJS.UI_Events.declare('Modules/KeyListener');
@@ -21,4 +30,12 @@ structureJS.UI_Events.declare('Modules/DragResize', ['DB']);
 structureJS.UI_Events.declare('DB');
 
 structureJS.DB.declare('Modules/IdbClient');
-structureJS.DB.declare('Modules/Mongo');
+structureJS.DB.declare('Modules/Mongo', ['DB2']);
+structureJS.DB.declare('DB2');
+
+structureJS.DB2.declare('Modules/IdbClient');
+structureJS.DB2.declare('Modules/Mongo', ['DB3']);
+structureJS.DB2.declare('DB3');
+
+structureJS.DB3.declare('Modules/IdbClient');
+structureJS.DB3.declare('Modules/Mongo');
